257ea93ef027e4eca5a8945695d796899b9263a5
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-11-23 09:24:57 +0700

    Update Login.php
    little modify

--- application/views/Login.php
+++ application/views/Login.php
@@ -114,26 +114,18 @@
 		<!-- /Main Content -->
 
 	</div>
-	<!-- /#wrapper -->
 
-	<!-- JavaScript -->
-
-	<!-- jQuery -->
 	<script src="<?php echo base_url() ?>assets/vendors/bower_components/jquery/dist/jquery.min.js"></script>
 
-	<!-- Bootstrap Core JavaScript -->
 	<script src="<?php echo base_url() ?>assets/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	<script
 		src="<?php echo base_url() ?>assets/vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js">
 	</script>
 
-	<!-- Slimscroll JavaScript -->
 	<script src="<?php echo base_url() ?>assets/dist/js/jquery.slimscroll.js"></script>
 
-	<!-- Fancy Dropdown JS -->
 	<script src="<?php echo base_url() ?>assets/dist/js/dropdown-bootstrap-extended.js"></script>
 
-	<!-- Init JavaScript -->
 	<script src="<?php echo base_url() ?>assets/dist/js/init.js"></script>
 </body>
 
