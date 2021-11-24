3b45488bbb8e7b3fdc6c5c708313ea0505abddd2
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-11-24 11:20:34 +0700

    Update coba1.php
    little modify

--- application/views/print/coba1.php
+++ application/views/print/coba1.php
@@ -4,24 +4,12 @@
 	<title>ari</title>
 </head>
 <body>
-
 	<?php
-
 	foreach ($data_two as $data2) { ?>
 	<?php echo $data2?>
-
-
-
-	
-	
-
-	
 	<?php } ?>
-
-
 </body>
 </html>
-
 <!--script src="<?= base_url() ?>resources/vendors/bower_components/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
